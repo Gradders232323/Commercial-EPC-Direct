2d0312fbaf1b23d08981affbbff73b1ff07e8937
Jamie Gradwell <jamie@Jamies-MacBook-Pro.local>
2026-08-11 12:07:20 +0100

    Add Commercial EPC London location page

--- app/location-data.ts
+++ app/location-data.ts
@@ -7,7 +7,7 @@ export const locationRegions: LocationRegion[] = [
   { name: "West Midlands", slug: "west-midlands", cities: ["Birmingham", "Coventry", "Wolverhampton", "Solihull", "Walsall", "Worcester", "Stoke-on-Trent", "Telford"] },
   { name: "East Midlands", slug: "east-midlands", cities: ["Nottingham", "Leicester", "Derby", "Northampton", "Lincoln", "Chesterfield", "Mansfield"] },
   { name: "East of England", slug: "east-of-england", cities: ["Cambridge", "Norwich", "Ipswich", "Peterborough", "Chelmsford", "Luton", "Watford", "Colchester"] },
-  { name: "London", slug: "london", cities: ["Central London", "Croydon", "Enfield", "Kingston", "Romford", "Bromley", "Ealing", "Harrow"] },
+  { name: "London", slug: "london", cities: ["London", "Central London", "Croydon", "Enfield", "Kingston", "Romford", "Bromley", "Ealing", "Harrow"] },
   { name: "South East", slug: "south-east", cities: ["Brighton", "Reading", "Oxford", "Southampton", "Portsmouth", "Guildford", "Milton Keynes", "Canterbury", "Maidstone"] },
   { name: "South West", slug: "south-west", cities: ["Bristol", "Bath", "Exeter", "Plymouth", "Bournemouth", "Swindon", "Gloucester", "Cheltenham", "Truro"] },
   { name: "Wales", slug: "wales", cities: ["Cardiff", "Swansea", "Newport", "Wrexham", "Bangor", "Aberystwyth"] },
